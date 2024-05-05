8fa8df5c48812a137686cb09be2f44d4ca03057c
Manish <iammanish0312@gmail.com>
2024-05-05 12:44:05 +0530

    backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -41,17 +41,6 @@ app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
 
-// app.use(authMiddleware);
-app.post("/api/v1/blog", (c) => {
-  return c.text("Hello Hono!");
-});
-app.get("/api/v1/blog", (c) => {
-  return c.json({ message: "token found" });
-});
-app.put("/api/v1/blog/:id", (c) => {
-  return c.text("Hello Hono!");
-});
-
 app.delete("/delete", async (c) => {
   const prisma = new PrismaClient({
     datasourceUrl: c.env.DATABASE_URL,
