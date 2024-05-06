d6bd7af42591ba1a6ffaaa927f0ce33933d126a3
Manish <iammanish0312@gmail.com>
2024-05-06 13:57:47 +0530

    added zod user/blog

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -2,6 +2,7 @@ import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 import { verify } from "hono/jwt";
+import { addBlogInput } from "@manish_dev/narrative-common";
 
 export const blogRouter = new Hono<{
   Bindings: {
@@ -43,6 +44,11 @@ blogRouter.post("/add-blog", async (c) => {
   }).$extends(withAccelerate());
   try {
     const body = await c.req.json();
+    const { success } = addBlogInput.safeParse(body);
+    if (!success) {
+      c.status(411);
+      return c.json({ message: "Inputs are not correct" });
+    }
     const userId = c.get("userId");
     const blog = await prisma.post.create({
       data: {
@@ -102,6 +108,11 @@ blogRouter.put("/:id", async (c) => {
   }).$extends(withAccelerate());
   try {
     const body = await c.req.json();
+    const { success } = addBlogInput.safeParse(body);
+    if (!success) {
+      c.status(411);
+      return c.json({ message: "Inputs are not correct" });
+    }
     const id = c.req.param("id");
     const blog = await prisma.post.update({
       where: {
