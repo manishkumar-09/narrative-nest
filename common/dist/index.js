2a79453ba1dfd355ef3a78af90699bfc7efdbc59
Manish <iammanish0312@gmail.com>
2024-05-06 13:07:41 +0530

    created npm package

---
+++ common/dist/index.js
@@ -0,0 +1,24 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.updateBlogInput = exports.addBlogInput = exports.signinInput = exports.signupInput = void 0;
+const zod_1 = __importDefault(require("zod"));
+exports.signupInput = zod_1.default.object({
+    name: zod_1.default.string().optional().default("Anonymous"),
+    email: zod_1.default.string().email(),
+    password: zod_1.default.string().min(6),
+});
+exports.signinInput = zod_1.default.object({
+    email: zod_1.default.string().email(),
+    password: zod_1.default.string().min(6),
+});
+exports.addBlogInput = zod_1.default.object({
+    title: zod_1.default.string(),
+    content: zod_1.default.string(),
+});
+exports.updateBlogInput = zod_1.default.object({
+    title: zod_1.default.string(),
+    content: zod_1.default.string(),
+});
